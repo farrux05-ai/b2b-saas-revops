7f96efd54e9210fa9f33451c5dc411e4495a16f9
Farrux Valijov <farruxbekvalijonov65@gmail.com>
2026-04-05 11:19:58 +0500

    feat: secure postgres macro with env vars and hide from docs

--- macros/postgres_source.sql
+++ macros/postgres_source.sql
@@ -1,7 +1,7 @@
 -- Macro to simplify reading from Postgres in DuckDB
 {%- macro postgres_source(schema, table) -%}
 postgres_scan(
-  'dbname=revops_database user=farrux password=farrux05 host=localhost port=5432',
+  'dbname={{ env_var("DBT_POSTGRES_DBNAME", "revops_database") }} user={{ env_var("DBT_POSTGRES_USER") }} password={{ env_var("DBT_POSTGRES_PASSWORD") }} host={{ env_var("DBT_POSTGRES_HOST") }} port={{ env_var("DBT_POSTGRES_PORT", "5432") }}',
   '{{ schema }}',
   '{{ table }}'
 )
